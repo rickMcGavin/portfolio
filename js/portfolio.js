9b8c5909846ea6fd2cfa8e0f48e1b50e990d32fc
rebelBeard <rfm144@gmail.com>
2017-02-15 23:16:17 -0500

    comments and example of how to build object

---
+++ js/portfolio.js
@@ -0,0 +1,12 @@
+
+// array of objects containing the data to build each card quickly
+const projecstObj = [
+	{
+		title: 'Tribute Page',
+		description: 'This page is a tribute to chess legend: Bobby Fischer.',
+		tools: 'HTML CSS Responsive Design'		
+	},
+]
+
+// loop through array and use string template to build the projects page on
+// init()
